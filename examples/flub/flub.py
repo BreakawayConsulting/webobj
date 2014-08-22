49b2b248030f8e181c9caa1d66fe38c85a64f9b3
Benno Leslie <benno@breakawayconsulting.com.au>
2014-08-22 21:12:51 +1000

    Basic support for WebObject rendering as JSON.
    Updated the 'flub' example to include a WebObject example.

--- examples/flub/flub.py
+++ examples/flub/flub.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
-from webobj import Server, Route, Data
+import time
+import threading
+from webobj import Server, Route, Data, WebObject
+
+class Flub(WebObject):
+    web_fields = ['bar']
+
+    def __init__(self):
+        self.bar = 1
+
+    def run(self):
+        while True:
+            time.sleep(1)
+            self.bar += 1
 
 
 def main(args):
+    flub = Flub()
+    flub_thread = threading.Thread(target=flub.run)
+    flub_thread.start()
+
     routes = [
         Route('/', Data(b'<html>Hello, Flub</html>')),
+        Route('/flub', flub),
     ]
     server = Server(routes)
     server.start()
--- webobj.py
+++ webobj.py
@@ -1,4 +1,5 @@
 import http.server
+import json
 import socketserver
 from collections import namedtuple
 
@@ -8,6 +9,18 @@ DEFAULT_ADDR = ('localhost', 8080)
 
 def first_matching(check, lst):
     return next(filter(check, lst))
+
+
+class WebObject:
+    web_fields = []
+
+    @property
+    def web_state(self):
+        return {fld: getattr(self, fld) for fld in self.web_fields}
+
+    @property
+    def web_data(self):
+        return json.dumps(self.web_state).encode('utf8')
 
 
 class Route(namedtuple('Route', ['route', 'content'])):
@@ -37,10 +50,18 @@ class Handler(http.server.BaseHTTPRequestHandler):
             route = first_matching(lambda x: x.matches(self), self.routes)
         except StopIteration:
             self.do_error(404)
+            return
 
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(route.content.data)
+        content = route.content
+
+        if isinstance(content, Data):
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(content.data)
+        elif isinstance(content, WebObject):
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(content.web_data)
 
 
 class Server:
