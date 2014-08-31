69fc3f5d97bf0feca724bbc8700aacb77995bc30
Benno Leslie <benno@breakawayconsulting.com.au>
2014-08-31 19:11:01 -0400

    Update the flub example to use the /flub+event event stream.

--- examples/flub/flub.js
+++ examples/flub/flub.js
@@ -1,6 +1,20 @@
 var Flub = React.createClass({
+    getInitialState: function() {
+        return {'bar': null};
+    },
+
+    componentWillMount: function() {
+        var feed = new EventSource("/flub+events");
+        feed.addEventListener("message", this.eventMsg, false);
+    },
+
+    eventMsg: function(msg) {
+        var flubState = JSON.parse(msg.data);
+        this.setState(flubState);
+    },
+
     render: function() {
-        return React.DOM.p({}, 'Hello, Flub!');
+        return React.DOM.p({}, 'Hello, Flub! Current count: ' + this.state.bar);
     }
 });
 
