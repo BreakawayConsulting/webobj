95fd6a46d64a003e95ef4d9a8d0b2a2544dfff2e
Benno Leslie <benno@breakawayconsulting.com.au>
2015-02-13 16:41:48 +1100

    Remove sys.path trickery from webobj.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import os
 from util import script
 from webobj import Route, Server, File
 import time
 import sys
 
 def main():
-    sys.path = [os.getcwd()] + sys.path
     module = __import__(sys.argv[1])
     server = Server(module.routes, module.authenticator)
     server.start()
