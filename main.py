1c778f5367706734bec8fda50382157b7f3144c7
Benno Leslie <benno@breakawayconsulting.com.au>
2015-02-16 00:14:26 +1100

    Add support for specifying the serving address on the command line.

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+from argparse import ArgumentParser
 from util import script
 from webobj import Route, Server, File
 import time
 import sys
 
 def main():
-    module = __import__(sys.argv[1])
-    server = Server(module.routes, getattr(module, 'authenticator', None))
+    parser = ArgumentParser(description='Logcat.')
+    parser.add_argument('module')
+    parser.add_argument('--addr', default='localhost:8080')
+
+    args = parser.parse_args(sys.argv[1:])
+    module = __import__(args.module)
+    addr_parts = args.addr.split(':')
+    host = addr_parts[0]
+    if len(addr_parts) == 1:
+        port = 80
+    else:
+        port = int(addr_parts[1])
+    addr = (host, port)
+
+    server = Server(module.routes, getattr(module, 'authenticator', None), addr)
     server.start()
 
 script()
